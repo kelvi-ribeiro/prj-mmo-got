bfac90437ebb16286815683046dbb085b83539f6
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2018-12-31 03:53:32 -0200

    Aula 93. Criando o controller de autenticacao

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,15 @@
 module.exports.index = function(application,req,res){
-    res.render('index')
+    res.render('index',{validacao:{},dadosForm:{}})
+}
+
+module.exports.autenticar = function(application,req,res){
+    var dadosForm = req.body;
+    req.assert('usuario','Usuário não pode ser vazio').notEmpty();
+    req.assert('senha','Senha não pode ser vazio').notEmpty();
+    var erros = req.validationErrors();
+    if(erros){
+        res.render('index',{validacao:erros,dadosForm:dadosForm});
+        return;
+    }
+    res.send('Tudo ok para criar a sessão');
 }
