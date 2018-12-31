336fed654a32ece828be98b90152e16f2b56ac9d
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2018-12-30 23:15:26 -0200

    Aula 86. Tela Cadastro de usuario - Recebendo e validando dados

--- app/controllers/cadastro.js
+++ app/controllers/cadastro.js
@@ -1,3 +1,19 @@
-module.exports.cadastro = function(app,req,res){
-    res.render('cadastro');
+module.exports.cadastro = function(application,req,res){
+    res.render('cadastro',{validacao:{},dadosForm:{}});
+}
+
+module.exports.cadastrar = function(application,req,res){
+    var dadosForm = req.body;
+    req.assert('nome','Nome Não pode ser vazio').notEmpty();
+    req.assert('nome','Nome deve entre 3 a 20 caracteres').len(3,20);
+    req.assert('usuario','Usuário Não pode ser vazio').notEmpty();
+    req.assert('usuario','Usuário deve entre 3 a 20 caracteres').len(3,20);
+    req.assert('senha','Senha Não pode ser vazio').notEmpty();
+    req.assert('casa','Casa Não pode ser vazio').notEmpty();
+    var erros = req.validationErrors();
+    if(erros){
+        res.render('cadastro',{validacao:erros, dadosForm:dadosForm});
+        return
+    }
+    res.send('Tudo certo');
 }
